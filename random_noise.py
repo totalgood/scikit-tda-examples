923fee2059f772f6f62079a144cf4333c2dd9cc1
hobs <hobs+github@totalgood.com>
2019-11-10 13:33:35 -0800

    example from medium

---
+++ random_noise.py
@@ -0,0 +1,6 @@
+import numpy as np
+from ripser import ripser
+from persim import plot_diagrams
+data = np.random.random((100,2))
+diagrams = ripser(data)['dgms']
+plot_diagrams(diagrams, show=True)
