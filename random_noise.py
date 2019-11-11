7e517a7d6b2af8f630ae14675e0b3cb0324a20fe
hobs <hobs+github@totalgood.com>
2019-11-11 14:57:14 -0800

    more understanding of tda

--- random_noise.py
+++ random_noise.py
@@ -2,44 +2,71 @@ import os
 import base64
 from io import BytesIO
 
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import seaborn  # noqa
+import matplotlib.pyplot as plt
+
 
 from ripser import ripser
-from persim import plot_diagrams
+import persim
 
-HTML_HEADER = """<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8"/>
-    </head>
+HTML_HEADER = """<!DOCTYPE HTML>
+<HTML lang="en">
+    <HEAD>
+        <META charset="utf-8"/>
+    </HEAD>
 """
-HTML_HEADER = """<!DOCTYPE html>"""
 HTML_FOOTER = """</html>"""
 
 
-def plot_to_html(fig=None, path='/midata/public/tda.html'):
+def fig_to_html(fig=None, path='/midata/public/tda.html', header='', footer=''):
     fig = fig or plt.gcf() or plt.figure()
     tmpfile = BytesIO()
     fig.savefig(tmpfile, format='png')
     encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
-    html = f"{HTML_HEADER}<img src='data:image/png;base64,{encoded}'>{HTML_FOOTER}".format(encoded)
+    html = f"{header}<img src='data:image/png;base64,{encoded}'>{footer}".format(encoded)
 
     if path:
         path = os.path.join(path, f'{__name__}_plot_to_html.html') if os.path.isdir(path) else path
-        with open(path,'w') as f:
+        with open(path, 'w') as f:
             f.write(html)
     return html
 
 
-def random_noise_tda(num_points=100, num_dimensions=2, shape=None, show=False):
+def generate_gmm(num_points=100, num_dimensions=2, means=[0], stds=[1], shape=None):
+    """ Gennerate Gaussian Mixture Model data points using the specified means and standard deviations
+
+    >>> df = generate_gmm((100, 5), means=[0, 3, 4, 5], stds=[1, .3, .4, .5])
+    >>> df.shape
+    (400, 5)
+    >>> df.mean()
+    """
+    shape = num_points if shape is None and isinstance(num_points, tuple) else shape
     if isinstance(shape, (tuple, list)):
         num_points, num_dimensions = shape
-    data = np.random.random((num_points, num_dimensions))
-    diagrams = ripser(data)['dgms']
-    plot_diagrams(diagrams, show=show)
-    return data, diagrams
+    data = []
+    for m, s in zip(means, stds):
+        data += list(m + s * np.random.randn((num_points, num_dimensions)))
+    return pd.DataFrame(data)
+
+
+def tda_to_html(data, show=False, plot_only=[0, 1, 2], max_dim=2):
+    topo = ripser(data, max_dim=2)
+    html = HTML_HEADER
+    html += '<TABLE>\n'
+    html += '  <tr>\n    <th>Parameter</th>\n    <th>Value</th>\n  </tr>\n'
+    for k in 'cocycles num_edges r_cover'.split():
+        html += f'  <tr>\n    <td>{k}</td>\n    <td>{topo[k]}</td>\n  </tr>\n'
+    html += '</TABLE>\n'
+    persim.plot_diagrams(topo['dgms'], show=show, plot_only=plot_only or None)
+    html += fig_to_html()
+    topo['html'] = html
+    return topo
+
 
 if __name__ == '__main__':
-    data, diagrams = random_noise_tda()
-    plot_to_html()
+    NUM_DIMENSIONS = 2
+    df = generate_gmm(100, NUM_DIMENSIONS)
+    df = generate_gmm()
+    tda_to_html()
